ea4364fe8dcf9aaa3a2e188a89c38757235fcf8f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-21 10:36:04 +0900

    Using the Ethers dependency to create the new wallet

--- src/wallet.js
+++ src/wallet.js
@@ -5,7 +5,12 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export function generateWallet() {
-
+    const wallet = Wallet.createRandom();
+    console.log("🪪 New Wallet Created!");
+    console.log("Address:", wallet.address);
+    console.log("Private Key:", wallet.privateKey);
+    console.log("Mnemonic:", wallet.mnemonic?.phrase || "(not available)");
+    return wallet;
 }
 
 export function loadWalletFromEnv() {
