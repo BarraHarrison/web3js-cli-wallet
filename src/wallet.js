d47f37eb57d380d67829a4e8823b17423d5d75ec
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-21 10:33:08 +0900

    functions to create and load the ethereum wallet

---
+++ src/wallet.js
@@ -0,0 +1,13 @@
+import { Wallet } from "ethers";
+import { getProvider } from "./provider.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export function generateWallet() {
+
+}
+
+export function loadWalletFromEnv() {
+
+}
