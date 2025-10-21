41ec8546fca02f20ec88233354aa8fb4373bfd63
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-21 10:39:32 +0900

    Loading an existing key from PRIVATE_KEY in the dotenv

--- src/wallet.js
+++ src/wallet.js
@@ -14,5 +14,15 @@ export function generateWallet() {
 }
 
 export function loadWalletFromEnv() {
+    const privateKey = process.env.PRIVATE_KEY;
 
+    if (!privateKey) {
+        throw new Error("❌ Missing PRIVATE_KEY in .env file");
+    }
+
+    const provider = getProvider();
+    const wallet = new Wallet(privateKey, provider);
+    console.log("🔐 Wallet loaded from .env");
+    console.log("Address:", wallet.address);
+    return wallet;
 }
