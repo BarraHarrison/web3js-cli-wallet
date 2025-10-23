353d61a90c12a56c9f4f93092a7b41c635bec55e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-23 11:16:14 +0900

    Function needs to be refactored

--- src/generate_wallet.js
+++ src/generate_wallet.js
@@ -4,12 +4,6 @@ import fs from "fs";
 async function generateWallet() {
     const wallet = ethers.Wallet.createRandom();
 
-    console.log("✅ New Wallet Generated!");
-    console.log("---------------------------");
-    console.log("Address:", wallet.address);
-    console.log("Private Key:", wallet.privateKey);
-    console.log("Mnemonic:", wallet.mnemonic?.phrase || "N/A");
-    console.log("---------------------------");
 }
 
 generateWallet();
